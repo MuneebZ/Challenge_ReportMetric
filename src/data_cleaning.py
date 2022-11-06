2ac074e8e5f1c49de43e0c1f94e486234e2e768d
Muneeb Zafar <muneeb-zafar@outlook.com>
2022-11-06 20:09:29 +0000

    Added a data cleaning  function

---
+++ src/data_cleaning.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+
+def df_cleaning():
+
+    # Load datasets
+    order = pd.read_csv('templates/data/orders.csv')
+    orderLine = pd.read_csv('templates/data/order_lines.csv')
+    # product = pd.read_csv('templates/data/products.csv')
+    promotion = pd.read_csv('templates/data/promotions.csv')
+    productProm = pd.read_csv('templates/data/product_promotions.csv')
+    vendorCom = pd.read_csv('templates/data/commissions.csv')
+
+    order['date_parsed'] = pd.to_datetime(
+        order['created_at'], infer_datetime_format=True)
+    order['date_parsed'] = pd.to_datetime(
+        order['date_parsed'].dt.date, infer_datetime_format=True)
+    vendorCom['date_parsed'] = pd.to_datetime(
+        vendorCom['date'], infer_datetime_format=True)
+    productProm['date_parsed'] = pd.to_datetime(
+        productProm['date'], infer_datetime_format=True)
+    df = pd.merge(left=orderLine, right=order, how="left",
+                  left_on='order_id', right_on='id')
+    df = pd.merge(left=df, right=vendorCom, how="left", left_on=['date_parsed', 'vendor_id'],
+                  right_on=['date_parsed', 'vendor_id'], )
+    df = pd.merge(left=df, right=productProm, how="left", left_on=['date_parsed', 'product_id'],
+                  right_on=['date_parsed', 'product_id'], )
+    df = pd.merge(left=df, right=promotion, how="left",
+                  left_on=['promotion_id'], right_on=['id'], )
+
+    return df
+
+
+df = df_cleaning()
