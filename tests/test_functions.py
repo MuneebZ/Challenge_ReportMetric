a4d29903462f1df0cd3c89e17f189ce5dc50b761
Muneeb Zafar <muneeb-zafar@outlook.com>
2022-11-07 00:48:49 +0000

    Added description to the class in test_functions.py

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,6 +2,10 @@ from typing import Any, SupportsRound
 
 
 class RoundErrorWarning(SupportsRound['RoundErrorWarning']):
+    """
+    Testing for flagged issue regarding rounding error for the
+    final metric values once the class ReportMetric has been initalised.
+    """
 
     def __round__(self: 'RoundErrorWarning', ndigits: int = 0) -> 'RoundErrorWarning':
         return self
@@ -15,4 +19,3 @@ def test_tmp() -> None:
     assert type(result) == RoundErrorWarning
     result = round(x)
     assert type(result) == RoundErrorWarning
-
